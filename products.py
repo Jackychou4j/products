9f405e56208db64afb5dd169cba509c6bd086075
Jacky Chou <jacky4jmain@gmail.com>
2021-01-29 18:06:29 +0800

    增加細項顯示價格說明

--- products.py
+++ products.py
@@ -10,4 +10,5 @@ while True:
     products.append(p) #上面更省略可以寫成 products.append([name, price])
 print("您的商品名稱與價格為:",products)
 
-print(products[0][0])
+for p in products:
+    print(p[0], "的價格是", p[1])
